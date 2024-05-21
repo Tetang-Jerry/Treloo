dc5c173c35ffc18c3aa06bebc25d7f926a7e3914
Tetang-Jerry <juanjerry120@gmail.com>
2024-05-21 13:33:35 +0100

    Ended hamburger submenu 1,2 and 3

--- src/assets/JS/submenu.js
+++ src/assets/JS/submenu.js
@@ -63,6 +63,38 @@ function submenu2_open() {
     currentSubmenu = 'submenu2'
 }
 
+function submenu3_open() {
+    let submenu = document.getElementById('submenu3');
+
+    submenu.classList.remove('translate-x-full');
+
+    let logo = document.getElementById('logo');
+
+    let back = document.getElementById("back")
+
+    if(back.classList.contains('translate-x-full')) {
+        back.classList.remove('translate-x-full')
+    }
+
+    if(submenu.classList.contains('hidden')) {
+        submenu.classList.remove('hidden')
+        
+    }
+
+    setTimeout(() => {
+        logo.classList.add('-translate-x-full');
+    },200)
+
+    setTimeout(() => {
+        logo.classList.add('opacity-0');
+        
+        back.classList.remove("opacity-0")
+    },300)
+
+    currentSubmenu = 'submenu3'
+}
+
+
 function close_submenu() {
     let submenu = document.getElementById(currentSubmenu);
 
