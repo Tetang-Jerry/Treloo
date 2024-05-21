8b43ab5b122dd7742e5e4f06672c68cb437ad87a
Tetang-Jerry <juanjerry120@gmail.com>
2024-05-21 13:46:27 +0100

    Ended whole mobile header

--- src/assets/JS/submenu.js
+++ src/assets/JS/submenu.js
@@ -94,6 +94,36 @@ function submenu3_open() {
     currentSubmenu = 'submenu3'
 }
 
+function submenu4_open() {
+    let submenu = document.getElementById('submenu4');
+
+    submenu.classList.remove('translate-x-full');
+
+    let logo = document.getElementById('logo');
+
+    let back = document.getElementById("back")
+
+    if(back.classList.contains('translate-x-full')) {
+        back.classList.remove('translate-x-full')
+    }
+
+    if(submenu.classList.contains('hidden')) {
+        submenu.classList.remove('hidden')
+        
+    }
+
+    setTimeout(() => {
+        logo.classList.add('-translate-x-full');
+    },200)
+
+    setTimeout(() => {
+        logo.classList.add('opacity-0');
+        
+        back.classList.remove("opacity-0")
+    },300)
+
+    currentSubmenu = 'submenu4'
+}
 
 function close_submenu() {
     let submenu = document.getElementById(currentSubmenu);
